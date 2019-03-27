07ecf568612f422f02a8b6b13f3adf0b7bd85ed7
Bob Cohen <bobcohen1969@yahoo.com>
2019-03-26 19:42:48 -0500

    Updated Deal event handler

--- PokerV2/Form1.cs
+++ PokerV2/Form1.cs
@@ -12,6 +12,8 @@ namespace PokerV2
 {
     public partial class Form1 : Form
     {
+
+        Card [] hand = new Card[5];
        
 
         public Form1()
@@ -30,11 +32,17 @@ namespace PokerV2
             Card dealt4 = deck.DealCard();
             Card dealt5 = deck.DealCard();
 
-            card1.Text = dealt1.DisplayCard();
-            card2.Text = dealt2.DisplayCard();
-            card3.Text = dealt3.DisplayCard();
-            card4.Text = dealt4.DisplayCard();
-            card5.Text = dealt5.DisplayCard();
+            hand[0] = dealt1;
+            hand[1] = dealt2;
+            hand[2] = dealt3;
+            hand[3] = dealt4;
+            hand[4] = dealt5;
+
+            card1.Text = hand[0].DisplayCard();
+            card2.Text = hand[1].DisplayCard();
+            card3.Text = hand[2].DisplayCard();
+            card4.Text = hand[3].DisplayCard();
+            card5.Text = hand[4].DisplayCard();
             
 
 
